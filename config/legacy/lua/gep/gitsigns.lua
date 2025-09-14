c63a1ea14b82e222b00187be8765ca02b2e886b9
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-09-14 23:36:51 +0200

    gitsigns: migrate from deprecated functions

--- config/legacy/lua/gep/gitsigns.lua
+++ config/legacy/lua/gep/gitsigns.lua
@@ -8,16 +8,14 @@ gs.setup {
 }
 
 require 'gep.utils'.register_maps {
-  { 'n',  '<space>gj',     gs.next_hunk },
-  { 'n',  '<space>gk',     gs.prev_hunk },
+  { 'n',  '<space>gj',     function() gs.nav_hunk 'next' end },
+  { 'n',  '<space>gk',     function() gs.nav_hunk 'prev' end },
   { 'n',  '<space>gs',     gs.stage_hunk },
   { 'x',  '<space>gs',     function() gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end },
   { 'n',  '<space>g<s-s>', gs.stage_buffer },
   { 'n',  '<space>gr',     gs.reset_hunk },
   { 'x',  '<space>gr',     function() gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end },
   { 'n',  '<space>g<s-r>', gs.reset_buffer },
-  { 'n',  '<space>gu',     gs.undo_stage_hunk },
-  { 'n',  '<space>gt',     gs.toggle_deleted },
   { 'n',  '<space>gp',     gs.preview_hunk_inline },
   { 'n',  '<space>g<s-p>', gs.preview_hunk },
   { 'n',  '<space>gb',     function() gs.blame_line { full = true } end },
