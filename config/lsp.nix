dde3b5df07fe4da401b5a23ca86ee9e3e5153b31
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-11-02 23:15:41 +0100

    lsp: migrate most of plugins.lsp to lsp

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,12 +1,17 @@
 {
-  plugins.lsp = {
-    enable = true;
+  lsp = {
     servers = {
-      elixirls.enable = true;
       jsonls.enable = true;
       lemminx.enable = true;
       taplo.enable = true;
       yamlls.enable = true;
+    };
+  };
+
+  plugins.lsp = {
+    enable = true;
+    servers = {
+      elixirls.enable = true;
     };
   };
 }
