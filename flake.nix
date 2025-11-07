f3f1f610770f97cbb13830a9147f50737da507da
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-11-08 00:11:38 +0100

    flake: add nix-community substituter in nixConfig

--- flake.nix
+++ flake.nix
@@ -93,4 +93,13 @@
         }
       );
     };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 }
