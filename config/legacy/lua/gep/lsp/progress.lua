f1ea93a37c715808523d78d127110dbe6fd8ae28
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-05-16 01:29:06 +0200

    progress: add comments

--- config/legacy/lua/gep/lsp/progress.lua
+++ config/legacy/lua/gep/lsp/progress.lua
@@ -1,9 +1,11 @@
 require 'lsp-progress'.setup {
-  client_format = function(client_name, _, series_messages)
+  -- don't display the spinner
+  client_format = function(client_name, _spinner, series_messages)
     return #series_messages > 0
       and ('[' .. client_name .. '] ' .. table.concat(series_messages, ', '))
       or nil
   end,
+  -- remove "LSP" from the beginning of the message
   format = function(client_messages)
     return #client_messages > 0
       and table.concat(client_messages, ' ')
