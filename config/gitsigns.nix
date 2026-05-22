5e97145603521ac2ad6f60028a645c9491a85e9c
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-05-22 13:11:32 +0200

    gitsigns: refactor keymaps

--- config/gitsigns.nix
+++ config/gitsigns.nix
@@ -13,91 +13,87 @@
     };
   };
 
-  keymaps =
-    let
-      gs = action: utils.luaFunction "require('gitsigns').${action}";
-    in
-    [
-      {
-        mode = "n";
-        key = "<space>gj";
-        action = gs "nav_hunk 'next'";
-      }
-      {
-        mode = "n";
-        key = "<space>gk";
-        action = gs "nav_hunk 'prev'";
-      }
-      {
-        mode = "n";
-        key = "<space>gs";
-        action = gs "stage_hunk()";
-      }
-      {
-        mode = "x";
-        key = "<space>gs";
-        action = gs "stage_hunk { vim.fn.line '.', vim.fn.line 'v' }";
-      }
-      {
-        mode = "n";
-        key = "<space>g<s-s>";
-        action = gs "stage_buffer()";
-      }
-      {
-        mode = "n";
-        key = "<space>gr";
-        action = gs "reset_hunk()";
-      }
-      {
-        mode = "x";
-        key = "<space>gr";
-        action = gs "reset_hunk { vim.fn.line '.', vim.fn.line 'v' }";
-      }
-      {
-        mode = "n";
-        key = "<space>g<s-r>";
-        action = gs "reset_buffer()";
-      }
-      {
-        mode = "n";
-        key = "<space>gp";
-        action = gs "preview_hunk_inline()";
-      }
-      {
-        mode = "n";
-        key = "<space>g<s-p>";
-        action = gs "preview_hunk()";
-      }
-      {
-        mode = [
-          "o"
-          "x"
-        ];
-        key = "ih";
-        action = gs "select_hunk()";
-      }
-      {
-        mode = [
-          "o"
-          "x"
-        ];
-        key = "ah";
-        action = gs "select_hunk()";
-      }
-      {
-        mode = "n";
-        key = "<space><space>gb";
-        action = gs "blame_line { full = true }";
-      }
-      {
-        mode = "n";
-        key = "<space>g<s-b>";
-        action = gs "blame()";
-      }
-      {
-        mode = "n";
-        key = "<space>gd";
-        action = gs "diffthis()";
-      }
-    ];
+  keymaps = [
+    {
+      mode = "n";
+      key = "<space>gj";
+      action = utils.luaFunction "require('gitsigns').nav_hunk('next')";
+    }
+    {
+      mode = "n";
+      key = "<space>gk";
+      action = utils.luaFunction "require('gitsigns').nav_hunk('prev')";
+    }
+    {
+      mode = "n";
+      key = "<space>gs";
+      action = utils.luaFunction "require('gitsigns').stage_hunk()";
+    }
+    {
+      mode = "x";
+      key = "<space>gs";
+      action = utils.luaFunction "require('gitsigns').stage_hunk({ vim.fn.line '.', vim.fn.line 'v' })";
+    }
+    {
+      mode = "n";
+      key = "<space>g<s-s>";
+      action = utils.luaFunction "require('gitsigns').stage_buffer()";
+    }
+    {
+      mode = "n";
+      key = "<space>gr";
+      action = utils.luaFunction "require('gitsigns').reset_hunk()";
+    }
+    {
+      mode = "x";
+      key = "<space>gr";
+      action = utils.luaFunction "require('gitsigns').reset_hunk({ vim.fn.line '.', vim.fn.line 'v' })";
+    }
+    {
+      mode = "n";
+      key = "<space>g<s-r>";
+      action = utils.luaFunction "require('gitsigns').reset_buffer()";
+    }
+    {
+      mode = "n";
+      key = "<space>gp";
+      action = utils.luaFunction "require('gitsigns').preview_hunk_inline()";
+    }
+    {
+      mode = "n";
+      key = "<space>g<s-p>";
+      action = utils.luaFunction "require('gitsigns').preview_hunk()";
+    }
+    {
+      mode = [
+        "o"
+        "x"
+      ];
+      key = "ih";
+      action = utils.luaFunction "require('gitsigns').select_hunk()";
+    }
+    {
+      mode = [
+        "o"
+        "x"
+      ];
+      key = "ah";
+      action = utils.luaFunction "require('gitsigns').select_hunk()";
+    }
+    {
+      mode = "n";
+      key = "<space><space>gb";
+      action = utils.luaFunction "require('gitsigns').blame_line({ full = true })";
+    }
+    {
+      mode = "n";
+      key = "<space>g<s-b>";
+      action = utils.luaFunction "require('gitsigns').blame()";
+    }
+    {
+      mode = "n";
+      key = "<space>gd";
+      action = utils.luaFunction "require('gitsigns').diffthis()";
+    }
+  ];
 }
