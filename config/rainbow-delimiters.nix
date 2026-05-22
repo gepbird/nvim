6e09394af8d8707183f89d0119634f21ea84895b
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-05-23 01:05:10 +0200

    rainbow-delimiters: disable for big files
    Newer tree-sitter version doesn't have disable config.

--- config/rainbow-delimiters.nix
+++ config/rainbow-delimiters.nix
@@ -1,4 +1,5 @@
 {
+  utils,
   ...
 }:
 
@@ -11,6 +12,14 @@
         "RainbowDelimiterPurple"
         "RainbowDelimiterBlue"
       ];
+      strategy."" = utils.lua ''
+        function(bufnr)
+          if utils.is_file_big(bufnr) then
+            return nil
+          end
+          return require("rainbow-delimiters").strategy.global
+        end
+      '';
     };
   };
 
--- config/treesitter.nix
+++ config/treesitter.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  utils,
   ...
 }:
 
@@ -11,20 +10,10 @@
     settings = {
       highlight = {
         enable = true;
-        disable = utils.lua ''
-          function(_, bufnr)
-            return utils.is_file_big(bufnr)
-          end
-        '';
         additional_vim_regex_highlighting = false;
       };
       indent = {
         enable = true;
-        disable = utils.lua ''
-          function(_, bufnr)
-            return utils.is_file_big(bufnr)
-          end
-        '';
       };
     };
   };
