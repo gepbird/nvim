1f186ea0984d29a789403e80c1e9ba624bee552c
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-05-23 01:07:57 +0200

    treesitter: update to newer version

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -1,20 +1,9 @@
 {
-  pkgs,
-  ...
-}:
-
-{
   plugins.treesitter = {
     enable = true;
-    package = pkgs.vimPlugins.nvim-treesitter-legacy;
     settings = {
-      highlight = {
-        enable = true;
-        additional_vim_regex_highlighting = false;
-      };
-      indent = {
-        enable = true;
-      };
+      highlight.enable = true;
+      indent.enable = true;
     };
   };
 }
