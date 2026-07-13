454de9c1f48955defa1c57d46d35e699e6204778
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-07-14 00:05:16 +0200

    nightly: add fastBuild option

--- config/nightly.nix
+++ config/nightly.nix
@@ -1,9 +1,22 @@
 {
   neovim-nightly,
   pkgs,
+  lib,
   ...
 }:
 
+let
+  fastBuild = false;
+in
 {
-  package = neovim-nightly.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  package = neovim-nightly.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (
+    old:
+    lib.optionalAttrs fastBuild {
+      doCheck = false;
+      doInstallCheck = false;
+      cmakeFlags = old.cmakeFlags ++ [
+        (lib.cmakeBool "ENABLE_LTO" false)
+      ];
+    }
+  );
 }
