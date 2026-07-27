642d23b898229b55deea1fef607d91268c979f25
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-07-27 15:12:05 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/e2587caef70cea85dd97d7daab492899902dbf5d' (2026-07-23)
      → 'github:nixos/nixpkgs/624af665418d3c65d544145b4d34ad696439570e' (2026-07-26)
    • Removed input 'nixpkgs-patch-stylelint-lsp-fix-build'
    • Updated input 'nixvim':
        'github:nix-community/nixvim/f316e949e0ed9df0e1e0bf645c6dce721d4e230e' (2026-07-23)
      → 'github:nix-community/nixvim/48409beb41e8e28b64e8160ca82d8a93fb628963' (2026-07-25)

--- flake.nix
+++ flake.nix
@@ -6,10 +6,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-patcher.url = "github:gepbird/nixpkgs-patcher";
-    nixpkgs-patch-stylelint-lsp-fix-build = {
-      url = "https://github.com/NixOS/nixpkgs/pull/530554.diff";
-      flake = false;
-    };
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
